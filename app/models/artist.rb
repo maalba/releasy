6fd86c0566d10b5bda924008b993275759181bde
Hillel Solomon <solomonhillel@gmail.com>
2021-06-15 15:12:20 +1000

    added search to artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: :name,
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
