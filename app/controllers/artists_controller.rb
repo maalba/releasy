9660850f5bd2fb2eb2abce36f43947577c123c51
Hillel Solomon <solomonhillel@gmail.com>
2021-06-16 15:24:13 +1000

    search with spotify and add to local db

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,13 @@ class ArtistsController < ApplicationController
 
   def index
     if params[:query].present?
+      @query = params[:query]
       @artists = Artist.search_by_name(params[:query])
+      spotify_artists = RSpotify::Artist.search(params[:query])
+      @spotify_artists = []
+      spotify_artists.each do |artist|
+        @spotify_artists << Artist.new(name: artist.name, spotify_id: artist.id, image_url: artist.images.first ? artist.images.first["url"] : "https://images.unsplash.com/photo-1614680376593-902f74cf0d41?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80")
+      end
     else
       @artists = Artist.all
     end
@@ -19,6 +25,18 @@ class ArtistsController < ApplicationController
   def toggle_follow
     @artist = Artist.find(params[:id])
     current_user.favorited?(@artist) ? current_user.unfavorite(@artist) : current_user.favorite(@artist)
+  end
+
+  def new_follow
+    artist = Artist.new(artist_params)
+    artist.save!
+    current_user.favorite(artist)
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :spotify_id, :image_url)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  post 'new_follow', to: 'artists#new_follow'
   resources :artists, only: [ :index, :create ] do
     member do
       post 'toggle_follow', to: 'artists#toggle_follow'
