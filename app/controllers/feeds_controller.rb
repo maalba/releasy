68ee794888d434e6df8e231b52a0f89c919b7b5b
Hillel Solomon <solomonhillel@gmail.com>
2021-06-18 00:31:43 +1000

    fixed dashboard issue with spotify cards showing up

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,5 +3,6 @@ class FeedsController < ApplicationController
     @followed_artists = current_user.all_favorited
     @album_releases = @followed_artists.map { |artist| artist.albums }.flatten
     @album_releases.sort_by! { |album| album.release_date }.reverse!
+    @today = Date.today
   end
 end
