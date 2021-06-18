6884bd98c133d18bd52d14c5cb72975ea611ab36
Hillel Solomon <solomonhillel@gmail.com>
2021-06-18 14:36:46 +1000

    included messages with links in feed and dashboard if nothing there

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   def show
     if params[:query].present?
       @followed_artists = Artist.search_by_name(params[:query]).select { |artist| current_user.favorited?(artist) }
+      @query = params[:query]
     else
       @followed_artists = current_user.all_favorited
     end
