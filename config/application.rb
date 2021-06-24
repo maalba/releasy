3f61a25607ab40f1af263e89d143445067ebe41d
Matt Baddour <mattbaddour@gmail.com>
2021-06-24 13:02:37 +1000

    config assets digest true

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Releasy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     RSpotify::authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+    config.assets.digest = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
