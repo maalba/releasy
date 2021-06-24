37c37d4920bf1d69dcd08051f40149f9856d0c64
Matt Baddour <mattbaddour@gmail.com>
2021-06-24 13:10:34 +1000

    undoing a test in application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Releasy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     RSpotify::authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
-    config.assets.digest = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
